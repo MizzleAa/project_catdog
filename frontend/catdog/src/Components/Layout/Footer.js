30ad564a40dedae4c075f4d1c95346723917be25
MizzleNA <mizzleaaa@gmail.com>
2021-07-13 00:38:34 +0900

    :memo: update footer.

--- frontend/catdog/src/Components/Layout/Footer.js
+++ frontend/catdog/src/Components/Layout/Footer.js
@@ -9,10 +9,8 @@ function Footer(){
     return(
         <div>
             <div className="flex w-full h-48 bg-gray-100">
-                <div className="w-1/2 sm:w-full md:w-1/2 lg:w-1/2 h-48 flex items-center justify-center text-gray-400">
-                    <label className="w-full text-center">© 2021 MizzleNA, Predict Catdog 🐈🦮. </label>
-                </div>
-                <div className="w-1/2 sm:w-full md:w-1/2 lg:w-1/2 h-48 flex items-center justify-center text-gray-400 text-center">
+                <div className="w-full sm:w-full h-48 flex items-center justify-center text-gray-400">
+                    <label className="text-center pr-12">© 2021 MizzleNA, Predict Catdog 🐈🦮. </label>
                     <img className="bg-gray-200 rounded h-6 w-6 m-2" src={Facebook} alt="facebook"/>
                     <img className="bg-gray-200 rounded h-6 w-6 m-2" src={Google} alt="google"/>
                     <img className="bg-gray-200 rounded h-6 w-6 m-2" src={Twitter} alt="twitter"/>
